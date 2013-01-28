decc7b87f89e712c634400cfab63307ec1640a69
Jason Wolfe <jason@w01fe.com>
2013-01-27 19:51:27 -0800

    Add license and url to project.clj

--- project.clj
+++ project.clj
@@ -1,2 +1,6 @@
 (defproject plumbing "0.0.1-SNAPSHOT"
-  :description "Prismatic's Clojure utility belt.")
+  :description "Prismatic's Clojure utility belt."
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
+  :url "https://github.com/Prismatic/plumbing")
