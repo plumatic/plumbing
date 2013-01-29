356b4f09c54bf9a7a33bff8b598902238335cce2
Jason Wolfe <jason@w01fe.com>
2013-01-29 00:01:17 -0800

    Update project.clj to 0.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prismatic/plumbing "0.0.1"
+(defproject prismatic/plumbing "0.0.2-SNAPSHOT"
   :description "Prismatic's Clojure utility belt."
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
