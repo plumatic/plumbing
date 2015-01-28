6b5a1d2afe9aaa83cb4babb24367880541e04b37
loganlinn <logan.linn@gmail.com>
2015-01-27 21:05:22 -0800

    lein-release plugin: preparing 0.3.7 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prismatic/plumbing "0.3.7-SNAPSHOT"
+(defproject prismatic/plumbing "0.3.7"
   :description "Prismatic's Clojure utility belt."
   :url "https://github.com/Prismatic/plumbing"
   :license {:name "Eclipse Public License - v 1.0"
