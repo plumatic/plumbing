plumatic/plumbing

commit 961fa0d5478074eb1659bcf6644c4b778c7fad17

Attach default values as metadata on optional key schemas

src/plumbing/fnk/impl.clj
@@ -92,7 +92,7 @@
           (assert-unschematized binding)
           (schema/assert-iae (= 1 (count schema-fixed-binding))
                              "optional binding has more than 1 entry: %s" schema-fixed-binding)
-          {:schema-entry [`(s/optional-key ~bound-key) (schema-macros/extract-schema-form bound-sym)]
+          {:schema-entry [`(with-meta (s/optional-key ~bound-key) {:default ~opt-val-expr}) (schema-macros/extract-schema-form bound-sym)]
            :body-form `(let [~(name-sym bound-sym) (get ~map-sym ~bound-key ~opt-val-expr)]
                          ~body-form)})
 

test/plumbing/core_test.cljx
@@ -503,7 +503,16 @@
       (is (= {:a s/Str :b {:c s/Str} s/Keyword s/Any} (pfnk/input-schema f)))
       (is (= ["1" "2"] (f {:a "1" :b {:c "2"}})))
       (is (thrown? Exception (f {:a "1" :b {:c 2}})))
-      (is (thrown? Exception (f {:a "1" :b {:c "2" :d "3"}}))))))
+      (is (thrown? Exception (f {:a "1" :b {:c "2" :d "3"}})))))
+
+  (testing "default values"
+    (is (= {:default "foo"}
+           (-> (p/fnk [{a :- s/Str "foo"}])
+               pfnk/input-schema
+               (dissoc s/Keyword)
+               keys
+               first
+               meta)))))
 
 (deftest fnk-qualified-key-test
   (is (= [1 2 3] ((p/fnk [a/b b/c c/d] [b c d]) {:a/b 1 :b/c 2 :c/d 3})))

src/plumbing/fnk/schema.cljx
@@ -86,7 +86,11 @@
         ;; Deal with `(s/optional-key k) form from impl
         (and (sequential? k) (not (vector? k)) (= (count k) 2)
              (= (first k) 'schema.core/optional-key))
-        [(second k) false]))
+        [(second k) false]
+
+        ;; Deal with `(with-meta ...) form from impl
+        (and (sequential? k) (not (vector? k)) (= (first k) `with-meta))
+        (unwrap-schema-form-key (second k))))
 
 (s/defn explicit-schema-key-map :- {s/Keyword s/Bool}
   "Given a possibly-unevaluated map schema, return a map from bare keyword to true

commit bac47df83285e328c81102e666bb306039e2ddc9

Fix dependent optional bindings (broken by 961fa0d54) and add test

src/plumbing/fnk/impl.clj
@@ -92,7 +92,7 @@
           (assert-unschematized binding)
           (schema/assert-iae (= 1 (count schema-fixed-binding))
                              "optional binding has more than 1 entry: %s" schema-fixed-binding)
-          {:schema-entry [`(with-meta (s/optional-key ~bound-key) {:default ~opt-val-expr}) (schema-macros/extract-schema-form bound-sym)]
+          {:schema-entry [`(with-meta (s/optional-key ~bound-key) {:default '~opt-val-expr}) (schema-macros/extract-schema-form bound-sym)]
            :body-form `(let [~(name-sym bound-sym) (get ~map-sym ~bound-key ~opt-val-expr)]
                          ~body-form)})
 

test/plumbing/core_test.cljx
@@ -420,6 +420,9 @@
        (assoc om :a 4 :h 77)))
     (testing "both fnks called"
       (is (= @call-count 2)))
+    (testing "dependent optional values"
+      (is (= [1 2 3]
+             ((p/fnk [a {b (* a 2)} {c (inc b)}] [a b c]) {:a 1}))))
 
     #+clj
     (testing "positional-fn"
@@ -510,13 +513,11 @@
       (is (thrown? Exception (f {:a "1" :b {:c "2" :d "3"}})))))
 
   (testing "default values"
-    (is (= {:default "foo"}
-           (-> (p/fnk [{a :- s/Str "foo"}])
-               pfnk/input-schema
-               (dissoc s/Keyword)
-               keys
-               first
-               meta)))))
+    (let [first-key-meta (p/fn-> pfnk/input-schema (dissoc s/Keyword) keys first meta)]
+      (is (= {:default "foo"}
+             (first-key-meta (p/fnk [{a :- s/Str "foo"}]))))
+      (is (= {:default 'apple}
+             (first-key-meta (p/fnk [apple {a :- s/Str apple}])))))))
 
 (deftest fnk-qualified-key-test
   (is (= [1 2 3] ((p/fnk [a/b b/c c/d] [b c d]) {:a/b 1 :b/c 2 :c/d 3})))